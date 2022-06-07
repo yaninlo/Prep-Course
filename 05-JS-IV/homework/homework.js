b2f7c4ee7ab751575402c30f1660a5b78b8c9747
´´yaninlo´´ <yangidlopez@gmail.com>
2022-06-07 16:30:11 -0300

    terminado

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -6,6 +6,15 @@ function crearGato(nombre, edad) {
   // Agrega un método (funcion) llamado "meow" que devuelva el string "Meow!"
   // Devuelve el objeto
   // Tu código:
+  var objeto = {
+    nombre: nombre, //Aqui el primer nombre es la propiedad y el string "nombre" es el valor.
+    edad: edad,
+    meow: function() {
+      return "Meow!";
+   
+  }
+};
+return objeto
 }
 
 
